e625c8b07fefaa6ccad7f95d13b550587ef1e043
ORCA-AI101 <sb58nf@gmail.com>
2026-08-31 16:50:09 +0300

    أضفنا Templates

--- students/views.py
+++ students/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from students.models import Student   
 # Create your views here.
 def hello(request):
-    students = Student.objects.all() 
-    return HttpResponse(students)
+    students = Student.objects.all()
+    return render(request, 'students/hello.html', {'students':students})
